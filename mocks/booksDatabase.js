6a38eab31d6f3d4fe57e81fafff31711d0bf6e22
Javier <2190271@my.ipleiria.pt>
2020-07-28 15:38:50 +0200

    Add id to current mock

--- mocks/booksDatabase.js
+++ mocks/booksDatabase.js
@@ -1,4 +1,5 @@
 var booksList = [{
+    id: 1,
     name: "Harry Potter and the Philosopher's Stone",
     isbn: "9780747532743",
     author: {
@@ -7,6 +8,7 @@ var booksList = [{
     },
 },
 {
+    id: 2,
     name: "Sherlock: The Return of Sherlock Holmes",
     isbn: "9781849907606",
     author: {
@@ -15,6 +17,7 @@ var booksList = [{
     }
 },
 {
+    id: 3,
     name: "East of the Mountains",
     isbn: "9780156011044",
     author: {
