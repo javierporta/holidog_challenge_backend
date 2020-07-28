f031e3c89053b3d9e3832f2977a6604626dda8ac
Javier <2190271@my.ipleiria.pt>
2020-07-28 15:27:12 +0200

    Mock book database

---
+++ mocks/booksDatabase.js
@@ -0,0 +1,18 @@
+var booksList = [{
+    name: "Harry Potter and the Philosopher's Stone",
+    isbn: "9780747532743",
+    author: "Rowling, J. K.",
+},
+{
+    name: "Sherlock: The Return of Sherlock Holmes",
+    isbn: "9781849907606",
+    author: "Conan Doyle, Arthur",
+},
+{
+    name: "East of the Mountains",
+    isbn: "9780156011044",
+    author: "Guterson, David",
+},
+]
+
+exports.booksList = booksList;
