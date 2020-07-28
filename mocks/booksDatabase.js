b913aec84143f4075c61c64a9fa6d2bd54ed7ce0
Javier <2190271@my.ipleiria.pt>
2020-07-28 15:36:15 +0200

    Fix response model accordng requirements

--- mocks/booksDatabase.js
+++ mocks/booksDatabase.js
@@ -1,17 +1,26 @@
 var booksList = [{
     name: "Harry Potter and the Philosopher's Stone",
     isbn: "9780747532743",
-    author: "Rowling, J. K.",
+    author: {
+        firstName: "J. K.",
+        lastName: "Rowling"
+    },
 },
 {
     name: "Sherlock: The Return of Sherlock Holmes",
     isbn: "9781849907606",
-    author: "Conan Doyle, Arthur",
+    author: {
+        firstName: "Arthur",
+        lastName: "Conan Doyle"
+    }
 },
 {
     name: "East of the Mountains",
     isbn: "9780156011044",
-    author: "Guterson, David",
+    author: {
+        firstName: "David",
+        lastName: "Guterson"
+    }
 },
 ]
 
